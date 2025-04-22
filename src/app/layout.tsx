b477570010795d850aae67d11511e40ef18b3024
wansom <ochiengwarren10@gmail.com>
2025-04-22 08:52:45 +0300

    add tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Notifications } from "@/components/ui/Notifications";
 import AuthProvider from "@/providers/AuthProvider";
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+                  <head>
+             <meta name="google-site-verification" content="g1yOFnL9E89beutPCUKKscciPwMZMDXPiyhz8Fqh0Y0" />
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-J08Q7P3RH2`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-J08Q7P3RH2', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <AuthProvider>
           <Notifications/>
