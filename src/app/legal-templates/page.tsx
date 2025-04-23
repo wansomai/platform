89b0c14b230c6095644e9a8450137c3382885a21
wansom <ochiengwarren10@gmail.com>
2025-04-23 11:19:56 +0300

    update contact page and legal templates

--- src/app/legal-templates/page.tsx
+++
@@ -1,8 +0,0 @@
-const Page = () => {
-    return (  <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-        <div className="bg-white shadow-md rounded-lg p-8 max-w-md w-full"></div>
-        Here is the articles page
-        </div>);
-}
- 
-export default Page;
