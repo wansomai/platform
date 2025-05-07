503c5d6adfb6560b225edbf4f5349159d9c9f484
wansom <ochiengwarren10@gmail.com>
2025-05-07 16:08:27 +0300

    remove ent tag from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,21 +50,6 @@ export default function RootLayout({
           }}
         />
 
-<Script
-  id="twitter-pixel"
-  strategy="afterInteractive"
-  dangerouslySetInnerHTML={{
-    __html: `
-      !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
-      },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
-      a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
-      twq('config','pp93c');
-      twq('event', 'tw-pp93c-pp93e', {
-        phone_number: null // phone number in E164 standard
-      });
-    `
-  }}
-/>
       </head>
       <body className={inter.className}>
         <AuthProvider>
