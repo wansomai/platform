7c6417a26709695b5dfc77d003bb707664dd4e29
wansom <ochiengwarren10@gmail.com>
2025-10-28 16:16:50 +0300

    update subscription limitits for free plan

--- src/lib/subscription.ts
+++ src/lib/subscription.ts
@@ -21,8 +21,8 @@ export interface UserPlanInfo {
 
 // Default limits for free plan
 const FREE_PLAN_LIMITS: SubscriptionLimits = {
-  maxProjects: 11,
-  maxMessages: 115,
+  maxProjects: 2,
+  maxMessages: 10,
   hasProAccess: false,
 };
 
