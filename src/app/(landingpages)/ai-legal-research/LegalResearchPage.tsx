9e920bd426fd0276f4b466f7b118376bbc5f26ca
wansom <ochiengwarren10@gmail.com>
2025-12-09 11:59:12 +0300

    update src/app/page.tsx src/app/legal-documents/[slug]/DocumentDetails.tsx src/app/(landingpages)/ai-legal-research/LegalResearchPage.tsx

--- src/app/(landingpages)/ai-legal-research/LegalResearchPage.tsx
+++ src/app/(landingpages)/ai-legal-research/LegalResearchPage.tsx
@@ -29,7 +29,7 @@ const LegalResearchPage = () => {
 
       {/* Hero Section */}
       <section className="pt-24 md:pt-32 pb-16 bg-primary relative overflow-hidden">
-        <div className="container mx-auto px-5 lg:px-20 z-10 relative">
+        <div className="section-container z-10 relative">
           <div className="relative flex flex-col lg:flex-row gap-12 items-center">
             <div className="text-white lg:basis-1/2 text-center lg:text-left">
 
