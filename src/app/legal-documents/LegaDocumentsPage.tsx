a9aa0ac0c4083910752ebc865b31b5810847344c
wansom <ochiengwarren10@gmail.com>
2026-01-21 14:50:32 +0300

    Update style for legal documents section and page

--- src/app/legal-documents/LegaDocumentsPage.tsx
+++ src/app/legal-documents/LegaDocumentsPage.tsx
@@ -126,7 +126,7 @@ const LegalDocumentsPageClient = () => {
             <>
               {filteredPosts.length > 0 ? (
                 <>
-                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                  <div className="space-y-5">
                     {paginatedPosts.map((post) => (
                       <LegalDocCard key={post.id} post={post} type="all" />
                     ))}
--- src/components/home/legal-documents.tsx
+++ src/components/home/legal-documents.tsx
@@ -5,25 +5,16 @@ import { ArrowRight } from "lucide-react";
 
 const LegalDocCard = ({ post, type }: { post: any; type: string }) => {
   return (
-    <div className="bg-white rounded-lg overflow-hidden shadow-sm group h-full flex flex-col">
-      <div className="relative h-[240px] overflow-hidden">
-        {post.image ? (
-          <Image
-            src={post.image}
-            alt={post.title}
-            fill
-            className="object-cover transition-transform duration-500 group-hover:scale-110"
-          />
-        ) : (
-          <div className="w-full h-full bg-gray-200 flex items-center justify-center">
+    <div className="bg-white rounded-lg overflow-hidden shadow-sm group h-full flex">
+      
+         <div className=" bg-gray-200 flex items-center justify-center">
             <Image
             src={'/contract-sample.webp'}
             alt={post.title}
-            fill
+            height={300}
+            width={100}
             className="object-cover transition-transform duration-500 group-hover:scale-110"
           />
-          </div>
-        )}
       </div>
       <div className="p-8 flex flex-col flex-grow">
         <h3 
@@ -33,7 +24,7 @@ const LegalDocCard = ({ post, type }: { post: any; type: string }) => {
         <div className="flex justify-between items-center mt-auto">
           <Link 
             href={post.link} 
-            className="flex items-center text-[#2E1A47] font-jost group"
+            className="flex items-center text-amber-500 font-jost group"
           >
             Read More 
             <ArrowRight className="w-4 h-4 ml-2 group-hover:ml-3 transition-all" />
